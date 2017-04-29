c12d539b54af5929be519c2ad3f6009ca82ebda4
Ntisnotawizard <Nnamdi@pc-77-5.customer.westgate.vita.lan>
2017-04-30 00:48:41 +0100

    chat still not working, gonna try pod install again

--- FitBud/ChatViewController.swift
+++ FitBud/ChatViewController.swift
@@ -8,20 +8,54 @@
 
 import UIKit
 import Firebase
-import JSQMessagesViewController
+import JSQMessagesViewController 
 
 class ChatViewController: JSQMessagesViewController{
     
+    var messages = [JSQMessage]() // store various instances of jsqmessages
     
+    // create outgoing and incoming message bubbles
+    
+    //lazy var outgoingBubbleImageView: JSQMessagesBubbleImage = self.setupOutgoingBubble()
+    //lazy var incomingBubbleImageView: JSQMessagesBubbleImage = self.setupIncomingBubble()
+ 
     var channelRef: FIRDatabaseReference?
     var channel: Channel? {
         didSet {
             title = channel?.name
         }
     }
-
+    
+    // create new jsqmessage and save it to the data source
+    private func addMessage(withId id: String, name: String, text: String) {
+        if let message = JSQMessage(senderId: id, displayName: name, text: text) {
+            messages.append(message)
+        }
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count // return number of messages in each section
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = messages[indexPath.item] // retrieve message
+        if message.senderId == senderId { // check user status to return either outgoing or incoming image
+            return outgoingBubbleImageView
+        } else { // return incoming image view
+            return incomingBubbleImageView
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // No avatars
+        collectionView!.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
+        collectionView!.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
         
         // This sets the senderId based on the logged in Firebase user.
 
@@ -33,6 +67,20 @@ class ChatViewController: JSQMessagesViewController{
         // Dispose of any resources that can be recreated.
     }
     
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        // messages from someone else
+        addMessage(withId: "foo", name: "Mr.Bolt", text: "I am so fast!")
+        // messages sent from local sender
+        addMessage(withId: senderId, name: "Me", text: "I bet I can run faster than you!")
+        addMessage(withId: senderId, name: "Me", text: "I like to run!")
+        // animates the receiving of a new message on the view
+        finishReceivingMessage()
+    }
 
     /*
     // MARK: - Navigation
